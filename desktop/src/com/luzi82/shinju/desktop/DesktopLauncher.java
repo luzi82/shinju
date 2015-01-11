181fcb539a6b18f71b87e36ce0035c9b5314d848
Luzi82 <luzi82@gmail.com>
2015-01-11 15:27:15 +0800

    init project

---
+++ desktop/src/com/luzi82/shinju/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.luzi82.shinju.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.luzi82.shinju.ShinjuGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ShinjuGame(), config);
+	}
+}
