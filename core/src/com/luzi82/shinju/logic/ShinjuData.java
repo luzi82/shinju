45166f6dfba042ae77c10591db990a411ff89928
Luzi82 <luzi82@gmail.com>
2015-01-17 20:47:22 +0800

    add data

---
+++ core/src/com/luzi82/shinju/logic/ShinjuData.java
@@ -0,0 +1,14 @@
+package com.luzi82.shinju.logic;
+
+import java.util.HashMap;
+
+
+public class ShinjuData {
+
+	public long mTime;
+
+	public int mCoin0;
+
+	public HashMap<Integer,ShinjuDataGirl> mGirlMap;
+
+}
---
+++ core/src/com/luzi82/shinju/logic/ShinjuDataGirl.java
@@ -0,0 +1,11 @@
+package com.luzi82.shinju.logic;
+
+public class ShinjuDataGirl {
+
+	public int mGirlId;
+
+	public int mX;
+
+	public int mY;
+
+}
