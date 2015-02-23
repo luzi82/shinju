c27fe8b27993f0e27e2a2851d6fc040396c6261f
Luzi82 <luzi82@gmail.com>
2015-02-23 23:27:47 +0800

    add hero button

--- core/src/com/luzi82/shinju/PlayScreenUiGroup.java
+++ core/src/com/luzi82/shinju/PlayScreenUiGroup.java
@@ -1,11 +1,16 @@
 package com.luzi82.shinju;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class PlayScreenUiGroup extends Group {
 
@@ -13,11 +18,16 @@ public class PlayScreenUiGroup extends Group {
 
 	public Label label;
 	public Label turn;
+	public Image mHeroButton;
 
 	// public ScreenViewport viewport;
 
 	public PlayScreenUiGroup(ShinjuCommon common) {
 		iCommon = common;
+
+		mHeroButton = new Image(new Texture(Gdx.files.internal("img/icon_madoka.png")));
+		mHeroButton.addListener(mHeroButtonClickListener);
+		addActor(mHeroButton);
 
 		LabelStyle ls = new LabelStyle(common.font, Color.BLACK);
 		label = new Label("Prototype", ls);
@@ -37,9 +47,21 @@ public class PlayScreenUiGroup extends Group {
 	}
 
 	protected void sizeChanged() {
-		// getViewport().update(width, height, true);
 		label.setPosition(0, getHeight(), Align.topLeft);
 		turn.setPosition(getWidth(), getHeight(), Align.topRight);
+
+		int buttonGap = (int) Math.floor(Gdx.graphics.getPpcX() * (Math.sqrt(ShinjuCommon.PHI) - 1) / 2);
+		int buttonSize = (int) Math.floor(Gdx.graphics.getPpcX());
+
+		mHeroButton.setSize(buttonSize, buttonSize);
+		mHeroButton.setPosition(buttonGap, getHeight() - buttonGap, Align.topLeft);
 	}
+
+	public ClickListener mHeroButtonClickListener = new ClickListener() {
+		@Override
+		public void clicked(InputEvent event, float x, float y) {
+			Gdx.app.debug("AalGBxGj", "hero clicked");
+		}
+	};
 
 }
